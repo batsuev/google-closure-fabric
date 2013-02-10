0ed5b9d24fd6ebf644bfcabc6e16edbfd926803d
Aleksandr Batsuev <batsuev@gmail.com>
2013-02-10 20:39:26 +0900

    base setup.py and tests gag

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name = "google_closure_fabric",
+    version = "0.0.1",
+    author = "Aleksandr Batsuev",
+    author_email = "batsuev@gmail.com",
+    description = "TODO",
+    license = "BSD",
+    keywords = "google closure gjslint google_closure",
+    url = "https://github.com/batsuev/google-closure-fabric",
+    packages = ['gclosurefab', 'test'],
+    long_description = 'TODO',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        ],
+    requires=[
+        "fabric"
+    ]
+)
