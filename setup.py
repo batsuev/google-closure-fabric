bcab83f4293f1de5aa72bda11943aa31eb2fd4fe
Aleksandr Batsuev <batsuev@gmail.com>
2013-02-14 13:16:39 +0900

    minor README changes

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name="google_closure_fabric",
-    version="0.0.1",
+    version="0.0.2",
     author="Aleksandr Batsuev",
     author_email="batsuev@gmail.com",
-    description="TODO",
+    description="fabric module for simplifying google closure integration",
     license="BSD",
-    keywords="google closure gjslint google_closure",
+    keywords="google closure gjslint soy",
     url="https://github.com/batsuev/google-closure-fabric",
     packages=['google_closure_fabric'],
-    long_description='TODO',
+    long_description=open('README.md', 'r').read(),
     classifiers=[
         "Development Status :: 1 - Planning",
         "Topic :: Utilities",
