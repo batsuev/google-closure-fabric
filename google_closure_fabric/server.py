56eab8676bbcb150f721840b9529fc052c192866
Aleksandr Batsuev <batsuev@gmail.com>
2013-03-01 18:15:16 +0900

    deps building

--- google_closure_fabric/server.py
+++ google_closure_fabric/server.py
@@ -4,8 +4,9 @@ from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 class CustomServer(TCPServer):
 
-    def __init__(self, project_path, server_address, RequestHandlerClass):
+    def __init__(self, project_path, deps_builder, server_address, RequestHandlerClass):
         self.project_path = project_path
+        self.deps_builder = deps_builder
         TCPServer.__init__(self, server_address, RequestHandlerClass)
 
 
@@ -17,15 +18,25 @@ class RequestHandler(SimpleHTTPRequestHandler):
         self.path = path + self.path
         SimpleHTTPRequestHandler.do_GET(self)
 
+    def __get_deps(self):
+        deps_content = self.server.deps_builder.get_deps()
+
+        self.send_response(200)
+        self.send_header("Content-type", 'text/javascript')
+        self.end_headers()
+
+        self.wfile.write(deps_content)
+
     def do_GET(self):
         if self.path.startswith('/goog/'):
             self.__get_google_closure_file()
+        elif self.path == '/deps':
+            self.__get_deps()
         else:
             SimpleHTTPRequestHandler.do_GET(self)
 
-def serve(project_path, port = 8000):
-    print 'project path %s' % project_path
-    httpd = CustomServer(project_path, ("", port), RequestHandler)
+def serve(project_path, deps_builder, port = 8000):
+    httpd = CustomServer(project_path, deps_builder, ("", port), RequestHandler)
     print 'Server started: http://127.0.0.1:%s/' % port
     httpd.serve_forever()
 
